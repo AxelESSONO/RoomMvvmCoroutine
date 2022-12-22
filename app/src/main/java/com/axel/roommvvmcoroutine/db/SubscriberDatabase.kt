4cec2ca91a07cac901e25be6f1c4946f652b1777
axel-essono <naxelcdric@gmail.com>
2022-12-23 00:21:44 +0100

    Create subscriber database instance

---
+++ app/src/main/java/com/axel/roommvvmcoroutine/db/SubscriberDatabase.kt
@@ -0,0 +1,29 @@
+package com.axel.roommvvmcoroutine.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.axel.roommvvmcoroutine.utils.SUBSCRIBER_DATA_BASE
+
+@Database(entities = [Subscriber::class], version = 1)
+abstract class SubscriberDatabase : RoomDatabase() {
+
+    //abstract val subscriberDAO : SubscriberDAO
+
+    companion object{
+        @Volatile
+        private var INSTANCE : SubscriberDatabase? = null
+        fun getInstance(context : Context) : SubscriberDatabase{
+            var instance =  INSTANCE
+            if (instance == null){
+                instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    SubscriberDatabase::class.java,
+                    SUBSCRIBER_DATA_BASE
+                ).build()
+            }
+            return instance
+        }
+    }
+}
