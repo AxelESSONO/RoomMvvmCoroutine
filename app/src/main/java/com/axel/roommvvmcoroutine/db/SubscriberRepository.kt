edadcffe82982468432ea5701da26e6f3f79cfd3
axel-essono <naxelcdric@gmail.com>
2022-12-23 00:40:17 +0100

    Create subscriber repository

---
+++ app/src/main/java/com/axel/roommvvmcoroutine/db/SubscriberRepository.kt
@@ -0,0 +1,23 @@
+package com.axel.roommvvmcoroutine.db
+
+class SubscriberRepository(private val dao : SubscriberDAO) {
+
+   suspend fun insert(subscriber: Subscriber){
+       dao.insertSubscriber(subscriber)
+   }
+
+    suspend fun update(subscriber: Subscriber){
+        dao.updateSubscriber(subscriber)
+    }
+
+    suspend fun delete(subscriber: Subscriber){
+        dao.deleteSubscriber(subscriber)
+    }
+
+    suspend fun deleteAll(){
+        dao.deleteAllSubscriber()
+    }
+
+    suspend fun getAll() = dao.getAllSubscriber()
+
+}
